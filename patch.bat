4a3dfb8c8d6765eb58da58cede04209ffe1bfdd9
Nystrata <15365192+Nystrata@users.noreply.github.com>
2024-05-10 10:51:23 -0700

    PAL Support!
    Adds support for PAL!

--- patch.bat
+++ patch.bat
@@ -9,7 +9,10 @@ set errorflag=0
 set nodToolPath=.\tools\nodtool.v1.win64.exe
 set xdelta3Path=.\tools\xdelta-3.1.0-x86_64.exe
 
-set "knownHash=ce781ad1452311ca86667cf8dbd7d112"
+set "knownHashG2ME01=ce781ad1452311ca86667cf8dbd7d112"
+set "knownHashG2MP01=37fa7767fff54cd4c19bf0db30443520"
+set "gameversion="
+set "vcdiffdir="
 
 echo.
 echo # ECHOES WIDESCREEN HUD PATCHER #
@@ -18,10 +21,12 @@ echo # ECHOES WIDESCREEN HUD PATCHER #
 echo. && echo.
 echo ## START ##
 echo.
-echo To begin, make a copy of your Metroid Prime 2: Echoes ^(USA^) ^(G2ME01^) ISO and place it in ths same folder as this script.
+echo To begin, make a copy of your Metroid Prime 2: Echoes ISO and place it in ths same folder as this script.
 echo Make sure it is named: echoes.iso
 echo.
-echo Then,
+echo This will only work for NTSC-U G2ME01 and PAL G2MP01 versions of the game. (NTSC-J is not supported.)
+echo.
+echo Then, 
 echo Press any key to to patch and create echoes_widescreenhud.iso . . .
 pause > NUL
 
@@ -35,7 +40,7 @@ if exist .\echoes.iso (
 ) else (
     echo echoes.iso not found.
     echo.
-    echo Make a copy of your Metroid Prime 2: Echoes ^(USA^) ^(G2ME01^) ISO and place it in ths same folder as this script.
+    echo Make a copy of your Metroid Prime 2: Echoes ISO and place it in ths same folder as this script.
     echo Make sure it is named: echoes.iso
     echo.
     echo Press any key to try again . . .
@@ -47,22 +52,43 @@ echo.
 echo Verifying MD5 hash of echoes.iso...
 call .\tools\md5.bat .\echoes.iso md5
 echo.
+echo   NTSC-U ^(G2ME01^) ISO hash: %knownHashG2ME01%
+echo      PAL ^(G2MP01^) ISO hash: %knownHashG2MP01%
+echo.
 echo            echoes.iso hash: %md5%
-echo   Expected G2ME01 ISO hash: %knownHash%
 echo.
 
-if %md5% == %knownHash% (
-    echo Hash verified. Continuing...
-) else (
-    echo Warning: The hash of echoes.iso does not match the known expected MD5 hash for Metroid Prime 2: Echoes ^(G2ME01^).
-    echo This may be due to echoes.iso being an improperly dumped image, in a different image format, a different localized version of the game, etc.
-    echo While you may still continue, this process will likely fail.
-    echo.
-    echo Press any key to continue anyway . . .
-    pause > nul
-    echo.
+if %md5% == %knownHashG2ME01% (
+    echo Hash successfully verified for NTSC-U ^(G2ME01^)...
+    set "gameversion=ntscu"
+    goto extraction
+)
+if %md5% == %knownHashG2MP01% (
+    set "gameversion=pal"
+    echo Hash successfully verified for PAL ^(G2MP01^)...
+    goto extraction)
+
+echo Warning: The hash of echoes.iso does not match the known expected MD5 hash for Metroid Prime 2: Echoes NTSC-U ^(G2ME01^) or PAL ^(G2MP01^)
+echo This may be due to echoes.iso being an improperly dumped image, in a different image format, a different localized version of the game, etc.
+echo While you may still continue, this process will likely fail.
+echo.
+echo Press any key to continue anyway . . .
+pause > nul
+echo.
+echo Select the game version for echoes.iso:
+echo   N: NTSC-U ^(G2ME01^)
+echo   P: PAL ^(G2MP01^)
+choice /c np
+if %ERRORLEVEL% == 2 (
+    echo Selected PAL ^(G2MP01^)...
+    set "gameversion=pal"
+)
+if %ERRORLEVEL% == 1 (
+    echo Selected NTSC-U ^(G2ME01^)...
+    set "gameversion=ntscu"    
 )
 
+:extraction
 echo. && echo.
 echo ## EXTRACTING ORIGINAL IMAGE ##
 %nodToolPath% extract -f .\echoes.iso ".\imgfs"
@@ -76,8 +102,20 @@ if %ERRORLEVEL% neq 0 (
 )
 
 echo. && echo.
+
+
 echo ## PATCHING ##
-%xdelta3Path% -v -f -d -s ".\imgfs\files\GGuiSys.pak" ".\vcdiff\GGuiSys.pak.vcdiff" ".\GGuiSys.pak"
+
+echo %gameversion%
+
+if %gameversion% == ntscu (
+    set "vcdiffdir=G2ME01-NTSC"
+)
+if %gameversion% == pal (
+    set "vcdiffdir=G2MP01-PAL"
+)
+
+%xdelta3Path% -v -f -d -s ".\imgfs\files\GGuiSys.pak" ".\vcdiff\%vcdiffdir%\GGuiSys.pak.vcdiff" ".\GGuiSys.pak"
 if %ERRORLEVEL% neq 0 (
     set errorflag=1
     echo.
@@ -89,7 +127,7 @@ if %ERRORLEVEL% neq 0 (
 del ".\imgfs\files\GGuiSys.pak"
 copy ".\GGuiSys.pak" ".\imgfs\files\GGuiSys.pak"
 
-%xdelta3Path% -v -f -d -s ".\imgfs\files\Standard.ntwk" ".\vcdiff\Standard.ntwk.vcdiff" ".\Standard.ntwk"
+%xdelta3Path% -v -f -d -s ".\imgfs\files\Standard.ntwk" ".\vcdiff\%vcdiffdir%\Standard.ntwk.vcdiff" ".\Standard.ntwk"
 if %ERRORLEVEL% neq 0 (
     set errorflag=1
     echo.
@@ -101,9 +139,10 @@ if %ERRORLEVEL% neq 0 (
 del ".\imgfs\files\Standard.ntwk"
 copy ".\Standard.ntwk" ".\imgfs\files\Standard.ntwk"
 
+
 echo. && echo.
 echo ## BUILDING NEW IMAGE ##
-"%nodToolPath%" makegcn ".\imgfs" "echoes_widescreenhud.iso"
+"%nodToolPath%" makegcn ".\imgfs" "echoes_%gameversion%_widescreenhud.iso"
 if %ERRORLEVEL% neq 0 (
     set errorflag=1
     echo.
@@ -117,9 +156,9 @@ echo. && echo.
 echo ## ClEANING UP ##
 echo Deleting temporary files that are no longer needed...
 @echo on
-rmdir %~dp0imgfs /q /s
-del %~dp0GGuiSys.pak
-del %~dp0Standard.ntwk
+rmdir "%~dp0imgfs" /q /s
+del "%~dp0GGuiSys.pak"
+del "%~dp0Standard.ntwk"
 @echo off
 
 echo. && echo.
@@ -129,7 +168,7 @@ echo.
 if %errorflag% neq 0 (
     echo Something went wrong. echoes_widescreenhud.iso may have failed to be created properly.
 ) else (
-    echo echoes_widescreenhud.iso has been created.
+    echo echoes_%gameversion%_widescreenhud.iso has been created.
 )
 echo.
 echo Reminder: Remember to enable the Widescreen Gecko cheat-codes in the emulator or loader. 
